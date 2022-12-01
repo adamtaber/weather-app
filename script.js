901eee2c93d4c1732bb30a0fc88c56647d95951c
Adam Taber <altaber@gmail.com>
2022-12-01 14:06:17 -0600

    add basic styling

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const form = document.getElementById('form');
-const body = document.querySelector('body');
+const body = document.getElementById('weatherBox')
 const button = document.getElementById('unitButton');
 
 let city
@@ -8,6 +8,7 @@ let unit = 'imperial';
 let cityName = document.createElement('p');
 let weatherTemp = document.createElement('p');
 let weatherSky = document.createElement('p');
+let weatherIcon = document.createElement('img');
 
 button.addEventListener('click', () => {
   if (unit === 'imperial') {
@@ -36,11 +37,18 @@ const getWeatherData = async (city='london', unit='imperial') => {
     cityName.innerText = "City: " + weatherData.name;
     weatherTemp.innerText = "Temperature: " + weatherData.main.temp;
     weatherSky.innerText = "Sky: " + weatherData.weather[0].description;
+    let iconCode = weatherData.weather[0].icon
+    weatherIcon.src = `http://openweathermap.org/img/wn/${iconCode}@2x.png`
     body.append(cityName);
     body.append(weatherTemp);
     body.append(weatherSky);
+    body.append(weatherIcon);
   } catch (error) {
     console.error('Error:', error);
+    cityName.innerText = 'Please enter a valid city';
+    weatherTemp.innerText = '';
+    weatherSky.innerText = '';
+    weatherIcon.src = '';
   }
 }
 
