645d3c0b9a5bfde2fdcb2ee910fe99cb242205e0
Adam Taber <altaber@gmail.com>
2022-12-01 11:48:23 -0600

    display weather data

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-console.log('test')
+const form = document.getElementById('form');
+const body = document.querySelector('body');
+const button = document.getElementById('unitButton');
+
+let city
+let unit = 'imperial';
+
+let cityName = document.createElement('p');
+let weatherTemp = document.createElement('p');
+let weatherSky = document.createElement('p');
+
+button.addEventListener('click', () => {
+  if (unit === 'imperial') {
+    button.innerText = 'Switch to Fahrenheit';
+    unit = 'metric';
+    getWeatherData(city, unit);
+  } else if (unit === 'metric') {
+    button.innerText = 'Switch to Celsius';
+    unit = 'imperial';
+    getWeatherData(city, unit);
+  }
+})
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let fd = new FormData(form);
+  let obj = Object.fromEntries(fd);
+  city = obj.city.split(' ').join('+');
+  getWeatherData(city, unit);
+})
+
+const getWeatherData = async (city='london', unit='imperial') => {
+  try {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=140c670d01837c54d85eb92c71351828&units=${unit}`, {mode: 'cors'})
+    const weatherData = await response.json();
+    cityName.innerText = "City: " + weatherData.name;
+    weatherTemp.innerText = "Temperature: " + weatherData.main.temp;
+    weatherSky.innerText = "Sky: " + weatherData.weather[0].description;
+    body.append(cityName);
+    body.append(weatherTemp);
+    body.append(weatherSky);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+getWeatherData();
